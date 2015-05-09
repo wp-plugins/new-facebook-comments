511993dd6d01d3f28e457a03567860744fe0bfec
itayyab0o1 <itayyab0o1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-09 16:51:06 +0000

    000000000000000000
    git-svn-id: https://plugins.svn.wordpress.org/new-facebook-comments/tags/1.3@1156945 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- facebook-comments.php
+++ facebook-comments.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name:  New Facebook Comments
+Plugin Name: Facebook Comments
 Description:  Facebook comments makes it simple to add the Facebook comments system to your WordPress site. You can also insert the facebook comment box as a shortcode into any post, page or theme.
 Version: 1.3
 Author: Tayyab
